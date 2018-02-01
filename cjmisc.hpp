abeab96fb2721bf3ca03a08f0f1d6e03ba98d3cc
hongchu098 <hongchu098@aliyun.com>
2018-02-01 23:36:49 +0800

    add cjmisc.hpp

---
+++ cjmisc.hpp
@@ -0,0 +1,15 @@
+// Copyright 2018, hongchu098.  All rights reserved. 
+// 
+// Licensed under an MIT-style license that can be found in 
+// the LICENSE file at the top-level directory. 
+ 
+// Author: hongchu098 (hongchu098 at aliyun dot com) 
+// 
+
+#ifndef CJMISC_CJMISC_HPP_
+# define CJMISC_CJMISC_HPP_
+
+# include "fixedint.hpp"
+# include "outputf.hpp"
+
+#endif
